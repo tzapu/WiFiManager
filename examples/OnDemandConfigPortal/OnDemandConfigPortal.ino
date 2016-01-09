7137082e74a2f697f36b09031df1d97b7b2ce461
Alberto Ricci Bitti <a.riccibitti@iname.com>
2016-01-09 17:50:09 +0100

    Removed redundant #include

--- examples/OnDemandConfigPortal/OnDemandConfigPortal.ino
+++ examples/OnDemandConfigPortal/OnDemandConfigPortal.ino
@@ -1,8 +1,6 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
 //needed for library
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
 
