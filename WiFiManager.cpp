951b39b08800da7e59237ec60ca16bb6c26d07be
Shawn A <spam@rumental.com>
2018-02-23 11:19:19 -0600

    fix param crash

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -923,7 +923,7 @@ String WiFiManager::getParamOut(){
         if(tok_n)pitem.replace(FPSTR(T_n), _params[i]->getID());
         if(tok_p)pitem.replace(FPSTR(T_p), FPSTR(T_t));
         if(tok_t)pitem.replace(FPSTR(T_t), _params[i]->getPlaceholder());
-        snprintf(parLength, 5, PSTR("%d"), _params[i]->getValueLength());
+        snprintf(parLength, 5, "%d", _params[i]->getValueLength());
         if(tok_l)pitem.replace(FPSTR(T_l), parLength);
         if(tok_v)pitem.replace(FPSTR(T_v), _params[i]->getValue());
         if(tok_c)pitem.replace(FPSTR(T_c), _params[i]->getCustomHTML()); // meant for additional attributes, not html
