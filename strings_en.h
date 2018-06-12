ab612bf9a3be14d19fa599f4fb384b5e45ef7de7
Stephan Veigl <stephan.veigl@gmail.com>
2018-06-12 14:04:27 +0200

    fix missing R_delete

--- strings_en.h
+++ strings_en.h
@@ -235,6 +235,7 @@ const char R_exit[]               PROGMEM = "/exit";
 const char R_close[]              PROGMEM = "/close";
 const char R_erase[]              PROGMEM = "/erase"; 
 const char R_status[]             PROGMEM = "/status";
+const char R_delete[]              PROGMEM = "/del"; 
 
 
 //Strings
