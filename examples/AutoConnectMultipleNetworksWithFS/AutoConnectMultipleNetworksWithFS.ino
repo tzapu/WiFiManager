c2b09892edf3f71eb2bf5beb03bd1ea5063db6ac
Stephan Veigl <stephan.veigl@gmail.com>
2018-06-13 11:57:05 +0200

    examples cleanup

--- examples/AutoConnectMultipleNetworksWithFS/AutoConnectMultipleNetworksWithFS.ino
+++ examples/AutoConnectMultipleNetworksWithFS/AutoConnectMultipleNetworksWithFS.ino
@@ -1,13 +1,6 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
-
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-
-//needed for library
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
-
 
 void load(WiFiManager &wifiManager) {
   //clean FS, for testing
