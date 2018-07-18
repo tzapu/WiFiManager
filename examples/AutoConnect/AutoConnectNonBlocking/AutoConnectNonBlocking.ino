74ceba740976b841aaeb18aea701b8de82039299
tablatronix <alverson@Shawns-MacBook-Pro.local>
2018-07-18 14:01:45 -0500

    add non blocking example

---
+++ examples/AutoConnect/AutoConnectNonBlocking/AutoConnectNonBlocking.ino
@@ -0,0 +1,26 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+WiFiManager wm;
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    //reset settings - wipe credentials for testing
+    //wm.resetSettings();
+
+    wm.setConfigPortalBlocking(false);
+
+    //automatically connect using saved credentials if they exist
+    //If connection fails it starts an access point with the specified name
+    if(wm.autoConnect("AutoConnectAP")){
+        Serial.println("connected...yeey :)");
+    }
+    else {
+        Serial.println("Configportal running");
+    }
+}
+
+void loop() {
+    wm.process();
+    // put your main code here, to run repeatedly:
+}
