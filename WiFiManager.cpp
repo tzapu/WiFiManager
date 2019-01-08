1282c4d5b520e447b51cbd9d107a72048b28724d
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-01-08 23:33:56 +0100

    wifimanager.cpp / wifimanager.h
    added define that checks on esp32 or esp8266. If another hardware is selectec, then do not try to compile this code.

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -12,6 +12,8 @@
 
 #include "WiFiManager.h"
 
+#if defined(ESP8266) || defined(ESP32)
+
 #ifdef ESP32
 uint8_t WiFiManager::_lastconxresulttmp = WL_IDLE_STATUS;
 #endif
@@ -2700,3 +2702,5 @@ void WiFiManager::WiFi_autoReconnect(){
     // }
   #endif
 }
+
+#endif
