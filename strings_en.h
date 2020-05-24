af3c3671a283f89fb501ed0b37669a4c23b4e77f
tablatronix <spam@rumental.com>
2020-05-24 13:19:00 -0500

    oops, remove js alert debug

--- strings_en.h
+++ strings_en.h
@@ -20,7 +20,7 @@ const char HTTP_HEAD_START[]       PROGMEM = "<!DOCTYPE html><html lang='en'><he
 const char HTTP_SCRIPT[]           PROGMEM = "<script>function c(l){"
 "document.getElementById('s').value=l.innerText||l.textContent;"
 "p = l.nextElementSibling.classList.contains('l');"
-"alert(p);"
+"// alert(p);"
 "document.getElementById('p').disabled = !p;"
 "if(p)document.getElementById('p').focus();}</script>";
 
