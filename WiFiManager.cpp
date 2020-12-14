565f9683e6a429cb5bd7fd53e03a41fd1140181a
tablatronix <alverson@mbpro.local>
2020-12-14 16:19:43 -0600

    fix captiveportal

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -2208,8 +2208,8 @@ boolean WiFiManager::captivePortal(AsyncWebServerRequest *request) {
   #endif
   if(!_enableCaptivePortal) return false; // skip redirections, @todo maybe allow redirection even when no cp ? might be useful
   
-  // String serverLoc =  toStringIp(request.client().localIP());
-  String serverLoc =  "192.168.4.1";
+  String serverLoc =  toStringIp(request->client()->localIP());
+  // String serverLoc =  "192.168.4.1";
   if(_httpPort != 80) serverLoc += ":" + (String)_httpPort; // add port if not default
   bool doredirect = serverLoc != request->host(); // redirect if hostheader not server ip, prevent redirect loops
   // doredirect = !isIp(server->hostHeader()) // old check
