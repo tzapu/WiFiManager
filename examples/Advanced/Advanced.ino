2001438d6fb1aea37045002d9973fc9fb4b9e636
tablatronix <alverson@mbpro.local>
2021-09-17 09:37:07 -0500

    fixing examples and actions

--- examples/Advanced/Advanced.ino
+++ examples/Advanced/Advanced.ino
@@ -8,7 +8,7 @@
 
 // wifimanager can run in a blocking mode or a non blocking mode
 // Be sure to know how to process loops with no delay() if using non blocking
-bool wm_nonblocking = false;
+bool wm_nonblocking = false; // change to true to use non blocking
 
 WiFiManager wm; // global wm instance
 WiFiManagerParameter custom_field; // global param ( for non blocking w params )
