6b0b1a4a84be41be259ce23c1f122f69c80f05ba
ricardollreis <ricardollreis@gmail.com>
2022-01-06 16:18:04 +0000

    Enable Portal Timeout in not Blocking Portal
    Enable the use of setConfigPortalTimeout( ) & setConfigPortalBlocking(false) at same time, by calling process()

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -756,6 +756,10 @@ boolean WiFiManager::process(){
     #if defined(WM_MDNS) && defined(ESP8266)
     MDNS.update();
     #endif
+	
+    if(configPortalActive && !_configPortalIsBlocking){
+      if(configPortalHasTimeout()) shutdownConfigPortal();
+    }
 
     if(webPortalActive || (configPortalActive && !_configPortalIsBlocking)){
         uint8_t state = processConfigPortal();
