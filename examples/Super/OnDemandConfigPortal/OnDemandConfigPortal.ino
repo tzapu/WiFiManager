91826229cb8e80c24d49ee32a243220c77f55c08
tablatronix <807787+tablatronix@users.noreply.github.com>
2022-03-26 13:01:01 -0500

    oops

--- examples/Super/OnDemandConfigPortal/OnDemandConfigPortal.ino
+++ examples/Super/OnDemandConfigPortal/OnDemandConfigPortal.ino
@@ -24,7 +24,6 @@ WiFiManager wm;
 
 // TEST OPTION FLAGS
 bool TEST_CP         = false; // always start the configportal, even if ap found
-bool TEST_CP         = true; // always start the configportal, even if ap found
 int  TESP_CP_TIMEOUT = 90; // test cp timeout
 
 bool TEST_NET        = true; // do a network test after connect, (gets ntp time)
