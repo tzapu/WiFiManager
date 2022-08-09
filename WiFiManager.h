04ef5ed1d3a4dc4458aaf969176f4cf4c30fe9cf
tablatronix <807787+tablatronix@users.noreply.github.com>
2022-08-09 18:07:14 -0500

    refactor strings, split out consts breaking!

--- WiFiManager.h
+++ WiFiManager.h
@@ -130,6 +130,7 @@
 #include <DNSServer.h>
 #include <memory>
 #include "strings_en.h"
+// #include "wm_consts.h"
 
 #ifndef WIFI_MANAGER_MAX_PARAMS
     #define WIFI_MANAGER_MAX_PARAMS 5 // params will autoincrement and realloc by this amount when max is reached
