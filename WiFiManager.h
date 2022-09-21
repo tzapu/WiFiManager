696b920273e8122cf4dff37d6f90e558950bfc7d
tablatronix <807787+tablatronix@users.noreply.github.com>
2022-09-20 22:11:52 -0500

    update rtc for resetreasons on multiple models

--- WiFiManager.h
+++ WiFiManager.h
@@ -109,7 +109,21 @@
     #endif
 
     #ifdef WM_RTC
-        #include <rom/rtc.h>
+        #ifdef ESP_IDF_VERSION_MAJOR // IDF 4+
+        #if CONFIG_IDF_TARGET_ESP32 // ESP32/PICO-D4
+        #include "esp32/rom/rtc.h"
+        #elif CONFIG_IDF_TARGET_ESP32S2
+        #include "esp32s2/rom/rtc.h"
+        #elif CONFIG_IDF_TARGET_ESP32C3
+        #include "esp32c3/rom/rtc.h"
+        #elif CONFIG_IDF_TARGET_ESP32S3
+        #include "esp32s3/rom/rtc.h"
+        #else
+        #error Target CONFIG_IDF_TARGET is not supported
+        #endif
+        #else // ESP32 Before IDF 4.0
+        #include "rom/rtc.h"
+        #endif
     #endif
 
 #else
